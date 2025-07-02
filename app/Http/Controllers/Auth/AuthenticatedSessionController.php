2131ac818a33be7dd7c17a765b430151ee183958
WaseemAlikhwan <waseemikhwan4@gmail.com>
2025-07-02 18:09:06 +0300

    Initial commit: User Registration API

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+ public function store(LoginRequest $request)
+{
+    $request->authenticate();
+
+    $token = $request->user()->createToken('auth-token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'user' => $request->user()
+    ]);
+}
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request)
+    {
+           $request->user()->currentAccessToken()->delete();
+
+    return response()->json([
+        'message' => 'Logged out successfully'
+    ]);
+    }
+}
